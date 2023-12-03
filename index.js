09f35e2441814cc116e18c9fec464177467b6975
ChArham777 <arhamch111@icloud.com>
2023-12-03 22:00:06 +0500

    Installed Mongoose And connected with mongo db with CRUD Operations

--- index.js
+++ index.js
@@ -1,77 +1,103 @@
-const express=require('express');
-const users=require('./MOCK_DATA.json')
-const app=express();
-const PORT=8000;
-app.use(express.json());
-app.listen(PORT,()=>console.log('SERVER STARTE AT PORT:${PORT}'));
-app.get("/", function (req, res) {
-    res.send("Hello World");
-  });
-  
-  app.get("/api/search", function (req, res) {
-    res.send("API Search");
-  });
-  
-  app.get("/api/users", function (req, res) {
-    res.send(users);
-  });
-  
-  app.get("/api/users/:id", function (req, res) {
-    users.forEach(element => {
-      if(
-      element.id === parseInt(req.params.id)
-      ){
-        res.send(element);
-      }
-    
-  });
-})
-  
-  app.put("/api/users/:id", function (req, res) {
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const users = require('./MOCK_DATA.json');
+const PORT = 8000;
 
-    users.forEach(element => {
-      if(
-      element.id === parseInt(req.params.id)
-      ){
-        element.first_name = req.body.first_name;
-        element.last_name = req.body.last_name;
-        element.email=req.body.email;
-        element.gender=req.body.gender;
-      
-        res.send(element);
-      }
-      else{
-        res.send('Not Foundd')
-      }
-      
+mongoose.connect("mongodb://127.0.0.1:27017/UsersDB", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connection Success"))
+  .catch((err) => console.log(err, "Error Connection"));
+
+const db = mongoose.connection;
+app.use(express.json());
+
+const usersSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  email: String,
+  gender: String,
+});
+
+const usersModel = mongoose.model('userModel', usersSchema);
+
+app.listen(PORT, () => console.log(`SERVER STARTED AT PORT:${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/search", (req, res) => {
+  res.send("API Search");
+});
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const allUsers = await usersModel.find();
+    res.send(allUsers);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.get("/api/users/:id", async (req, res) => {
+  try {
+    const user = await usersModel.findById(req.params.id);
+    if (!user) {
+      res.status(404).send('User not found');
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.put("/api/users/:id", async (req, res) => {
+  try {
+    const updatedUser = await usersModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        gender: req.body.gender,
+      },
+      { new: true }
+    );
+    if (!updatedUser) {
+      res.status(404).send('User not found');
+    } else {
+      res.send(updatedUser);
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.delete("/api/users/:id", async (req, res) => {
+  try {
+    const deletedUser = await usersModel.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      res.status(404).send('User not found');
+    } else {
+      res.send(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.post("/api/users", async (req, res) => {
+  try {
+    const newUser = new usersModel({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      gender: req.body.gender,
     });
-    
-    // const users = users.find(p => p.id === req.params.id);
-    // if (!users) return res.status(404).send("users not found");
-    // console.log(req.body.first_name)
-  
-  
-  });
-  
-  app.delete("/api/users/:id", function (req, res) {
-    const index = users.findIndex(p => p.id === parseInt(req.params.id));
-    if (index === -1) return res.status(404).send("users not found");
-  
-    users.splice(index, 1);
-    res.send(users);
-  });
-  
-  app.post("/api/users", function (req, res) {
-    const newUser = {
-      id: users.length + 1,
-      first_name : req.body.first_name,
-      last_name : req.body.last_name,
-      email:req.body.email,
-      gender:req.body.gender
-    };
-  
-    users.push(newUser);
-    res.send(newUser);
-  });
-  
-console.log(users);
+    const savedUser = await newUser.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
